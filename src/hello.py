cb81def342df44a59732ec1cda4b3c382ebeaeaa
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2020-07-20 11:08:18 +0200

    added testing

---
+++ src/hello.py
@@ -0,0 +1,20 @@
+def hello_world():
+    "prints hello world"
+    print("Hello World")
+    return
+
+
+def puss(name="Sonja"):
+    "prints puss + name"
+    print("puss" + name)
+    return
+
+
+def main():
+    Hello()
+    puss
+
+
+if __name__ == "__main__":
+    main()
+
